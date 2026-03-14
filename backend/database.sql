e294daa3d27dbdc92ea89cd3ff3af2c6437077e2
ramx10 <Shipalkarram@gmail.com>
2026-03-14 10:35:04 +0530

    feat: addproperties table

--- backend/database.sql
+++ backend/database.sql
@@ -6,3 +6,10 @@ CREATE TABLE users (
     role VARCHAR(20),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+CREATE TABLE properties (
+    id SERIAL PRIMARY KEY,
+    owner_id INT REFERENCES users(id),
+    property_name VARCHAR(100),
+    address TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
