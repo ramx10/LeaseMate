16e30afcdadfab618d52fac7aa494f340d3b69e7
ramx10 <Shipalkarram@gmail.com>
2026-03-14 10:57:08 +0530

    feat: connect Node.js backend with PostgreSQL database

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const pool = require("./db");
 
 const app = express();
 
@@ -8,6 +9,16 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("LeaseMate Backend Running");
+});
+
+app.get("/test-db", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json(result.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Database connection error");
+  }
 });
 
 const PORT = 5000;
