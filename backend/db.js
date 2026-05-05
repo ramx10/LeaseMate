5e35d118788b1baebf24012b6272c5568cacf78b
ramx10 <Shipalkarram@gmail.com>
2026-05-05 12:15:39 +0530

    Fix DB connection using host.docker.internal

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,7 @@
-const { Pool } = require("pg");
-
 const pool = new Pool({
   user: process.env.PGUSER || "postgres",
-  host: process.env.PGHOST || "localhost",
+  host: process.env.PGHOST || "host.docker.internal", // 🔥 CHANGE HERE
   database: process.env.PGDATABASE || "leasemate_db",
   password: process.env.PGPASSWORD || "1234",
   port: process.env.PGPORT || 5432,
 });
-
-module.exports = pool;
